55aaf429a2245d6c788aad8ea3475f6e548d6733
Isaias <isaiasrodrigues@Isaiass-MacBook-Air.local>
2023-08-15 17:32:08 -0300

    feat(add-reading-and-checking-current-data.): Add reading and checking of current data in the Main function

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ This module serves as the entry point for the Comunication-Instrument project. I
 temperature_collection module and checks if they are within permissible limits using the temperature_check module.
 """
 
+from data_collection.current_collection import collect_currents
 from data_collection.temperature_collection import collect_temperatures
+from utilities.current_check import check_current_threshold
 from utilities.temperature_check import (TemperatureTooHighError,
                                          check_temperatures)
 
-if __name__ == "__main__":
+
+def main():
+    """
+    Entry point for Communication Instrument design.
+
+    Collects and verifies that data (such as temperature, voltage,current) are within allowable limits.
+    """
     try:
         temperature_list = collect_temperatures()
         print("Collected Temperatures:", temperature_list)
@@ -17,3 +25,14 @@ if __name__ == "__main__":
         print("All temperatures are within limits.")
     except TemperatureTooHighError as e:
         print("Error:", e)
+
+    print("---------------------------------------------------")
+
+    currents = collect_currents()
+    print("Collected Current values:", currents)
+
+    for value in currents:
+        check_current_threshold(value)
+
+if _name_ == "_main_":
+    main()

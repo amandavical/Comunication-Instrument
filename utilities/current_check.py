556167955166d85c798a6eda83896a82233db939
Isaias <isaiasrodrigues@Isaiass-MacBook-Air.local>
2023-08-15 17:56:40 -0300

    feat(current_check): Addition of the function responsible for filtering the value of currents

---
+++ utilities/current_check.py
@@ -0,0 +1,11 @@
+"""Utilities for checking electric current values."""
+
+def check_current_threshold(value):
+    """
+    Contains a method that filters currents, leaving only those above 25mA.
+
+    parameter:
+        currents (int): List of currents.
+    """
+    if value < 25:
+        print("Threshold alert!")

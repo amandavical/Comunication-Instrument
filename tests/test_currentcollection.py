23cb13c2ffda0c496eb17c41ec21889b378f164f
Isaias <isaiasrd10@gmail.com>
2023-08-27 13:46:39 -0300

    fix(test_currentcollection.py): Changed function name 'test_collect_currents_above_limit' to 'test_collect_currents_at_limit'; modified in 'test_collect_currents_below_limit' the loop that handles the alert; Added class docstring

--- tests/test_currentcollection.py
+++ tests/test_currentcollection.py
@@ -1,27 +1,31 @@
 """
 Current Test Module.
 
+
 This module contains functions to simulate the collection of 30 random electric currents and the correct use of the
 values defined in the collect_currents and check_current_threshold functions.
 """
-import unittest
+
 import random
-from data_collection.current_collection import collect_currents
-from utilities.current_check import check_current_threshold
+import unittest
 from unittest.mock import MagicMock, patch
+
+from data_collection.current_collection import collect_currents
 
 
 class TestCurrentCollector(unittest.TestCase):
+    """
+    Test class.
+
+    In this class are added the test methods
+    """
     @patch("modules.comunication_client.Client")
     @patch("modules.simulated_instrument.Instrument")
-    @patch("data_collection.current_collection.check_current_threshold")
-    def test_collect_currents_above_limit(self, mock_check_current_threshold, mock_instrument, mock_client):
+    def test_collect_currents_at_limit(self, mock_instrument, mock_client):
         """
         Test collection of 30 random electrical currents between 0 and 2000.
-
         Returns:
-            list[int]: List containing electric currents values (0, 2000).
-
+        list[int]: List containing electric currents values (0, 2000).
         """
         mock_client_instance = MagicMock()
         mock_instrument_instance = MagicMock()
@@ -34,7 +38,7 @@ class TestCurrentCollector(unittest.TestCase):
         result = collect_currents()
 
         for current in result:
-            self.assertTrue(25 < current < 800)
+            self.assertTrue(25 < current < 2000)
             print(f"Collected currents: {current}")
 
     @patch("modules.comunication_client.Client")
@@ -45,8 +49,7 @@ class TestCurrentCollector(unittest.TestCase):
         Test collection of 30 random electrical currents between 0 and 24 for error message verification.
 
         Returns:
-            list[int]: List containing electric currents values (0, 24).
-
+        list[int]: List containing electric currents values (0, 24).
         """
         mock_client_instance = MagicMock()
         mock_instrument_instance = MagicMock()
@@ -59,12 +62,13 @@ class TestCurrentCollector(unittest.TestCase):
         result = collect_currents()
 
         with unittest.mock.patch('builtins.print') as mock_print:
-            check_current_threshold(24)
-            mock_print.assert_called_once_with("Threshold alert!")
-
             for current in result:
                 if current < 25:
-                    print(f"Threshold alert!")
+                    mock_check_current_threshold(current)
+                    mock_print.assert_called_once_with("Threshold alert!")
+                else:
+                    mock_check_current_threshold(current)
+                    mock_print.assert_not_called()
 
 
 if __name__ == "__main__":
